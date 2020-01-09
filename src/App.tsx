b3cb4e1d5b8768c741044d3cc6fa628abeaef063
Kenji Nagai <nagai-kj@nagai-galaxy.com>
2020-01-09 19:47:34 +0900

    公式のTODOをtypescript化 #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-const App: React.FC = () => {
+interface Item {
+  text: string;
+  id: number;
+}
+
+interface State {
+  items: Array<Item>;
+  text: string;
+}
+
+// https://reactjs.org/
+// class TodoApp extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { items: [], text: '' };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h3>TODO</h3>
+//         <TodoList items={this.state.items} />
+//         <form onSubmit={this.handleSubmit}>
+//           <label htmlFor="new-todo">
+//             What needs to be done?
+//           </label>
+//           <input
+//             id="new-todo"
+//             onChange={this.handleChange}
+//             value={this.state.text}
+//           />
+//           <button>
+//             Add #{this.state.items.length + 1}
+//           </button>
+//         </form>
+//       </div>
+//     );
+//   }
+//
+//   handleChange(e) {
+//     this.setState({ text: e.target.value });
+//   }
+//
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     if (!this.state.text.length) {
+//       return;
+//     }
+//     const newItem = {
+//       text: this.state.text,
+//       id: Date.now()
+//     };
+//     this.setState(state => ({
+//       items: state.items.concat(newItem),
+//       text: ''
+//     }));
+//   }
+// }
+//
+// class TodoList extends React.Component {
+//   render() {
+//     return (
+//       <ul>
+//         {this.props.items.map(item => (
+//           <li key={item.id}>{item.text}</li>
+//         ))}
+//       </ul>
+//     );
+//   }
+// }
+//
+// ReactDOM.render(
+//   <TodoApp />,
+//   document.getElementById('todos-example')
+// );
+
+const TodoList = React.memo((props: { items: Array<Item> }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+  );
+});
+
+const App: React.FC = () => {
+  const [state, setState] = useState({ items: [], text: '' } as State);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (!state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: state.text,
+      id: Date.now()
+    };
+    setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setState({ items: state.items, text: e.target.value });
+  };
+
+  return (
+    <div>
+      <h3>TODO</h3>
+      <TodoList items={state.items} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="new-todo">
+          What needs to be done?
+        </label>
+        <input
+          id="new-todo"
+          onChange={handleChange}
+          value={state.text}
+        />
+        <button>
+          Add #{state.items.length + 1}
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
